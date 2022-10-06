682eda7adac5bbae686c7be4c62f22fd31e026aa
chan <imchrg1@gmail.com>
2022-10-06 15:34:45 +0900

    Feature: session repository 구성 및 정의

---
+++ src/adapter/repository/Session.ts
@@ -0,0 +1,43 @@
+import { ISessionRepository } from '../../domain/repository/interface/ISession';
+import { IHttp } from '../infra/interface/IHttp';
+import { IStorage } from '../infra/interface/IStorage';
+import { ILoginDto } from '../../domain/dto/Login';
+
+export class SessionRepository implements ISessionRepository {
+  constructor(
+    private http: IHttp,
+    private storage: IStorage,
+  ) {}
+
+  async login(dto: ILoginDto): Promise<string | null> {
+    const { email, password } = dto;
+    const response = await this.http.request('http://localhost:3030/users/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: {
+        email,
+        password,
+      },
+    });
+
+    if(response.content) {
+      return response.content;
+    }
+
+    return null;
+  }
+
+  async getToken(): Promise<string | null> {
+    return await this.storage.get('token');
+  }
+
+  async setToken(token: string): Promise<void> {
+    await this.storage.set('token', token);
+  }
+
+  async removeToken(): Promise<void> {
+    await this.storage.remove('token');
+  }
+}
