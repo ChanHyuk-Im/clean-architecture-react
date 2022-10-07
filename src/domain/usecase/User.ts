1bfcf5351099f073f6c876f0e243709a0e4c10d3
chan <imchrg1@gmail.com>
2022-10-07 16:34:06 +0900

    Feature: user usecase에서 UserEntity 객체로 return

--- src/domain/usecase/User.ts
+++ src/domain/usecase/User.ts
@@ -1,3 +1,4 @@
+import { UserEntity } from '../entity/User';
 import { IUserEntity } from '../entity/interface/IUser';
 import { IUserUsecase } from './interface/IUser';
 import { IUserRepository } from '../repository/interface/IUser';
@@ -6,7 +7,19 @@ export class UserUsecase implements IUserUsecase {
   constructor(private userRepository: IUserRepository) {}
 
   async getUser(): Promise<IUserEntity | null> {
-    return await this.userRepository.getUser();
+    const user = await this.userRepository.getUser();
+
+    if(user) {
+      return new UserEntity({
+        id: user.id,
+        email: user.email,
+        name: user.name,
+        role: user.role,
+        createdAt: user.createdAt,
+      });
+    }
+
+    return null;
   }
 
   async setName(name: IUserEntity['name']): Promise<boolean> {
