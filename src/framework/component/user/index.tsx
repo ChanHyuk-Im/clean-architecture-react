ae8d664ff8e6e7ffdbf3388e0c7ba6802092ce2c
chan <imchrg1@gmail.com>
2022-10-07 15:00:52 +0900

    Feature: user component에 로그아웃 버튼 추가

--- src/framework/component/user/index.tsx
+++ src/framework/component/user/index.tsx
@@ -1,18 +1,25 @@
 import React from 'react';
 import { IUserEntity } from '../../../domain/entity/interface/IUser';
+import useStateStore from '../../store/state';
 import di from '../../di';
 
 const User = () => {
+  const { token, setToken } = useStateStore();
   const [userInfo, setUserInfo] = React.useState<IUserEntity | null>(null);
 
   React.useEffect(() => {
     init();
-  }, []);
+  }, [token]);
 
   const init = async () => {
     const _userInfo = await di.user.getUser();
 
     setUserInfo(_userInfo);
+  };
+
+  const onLogoutClick = async (e: React.MouseEvent<HTMLButtonElement>) => {
+    await di.session.removeToken();
+    setToken('');
   };
 
   return (
@@ -23,6 +30,7 @@ const User = () => {
         <div>{userInfo?.name}</div>
         <div>{userInfo?.role}</div>
         <div>{userInfo?.createdAt.toString()}</div>
+        <button onClick={onLogoutClick}>로그아웃</button>
       </>
     </div>
   );
